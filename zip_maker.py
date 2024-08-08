27cad17b559fdb2d278e49559af93d60cce1cc88
Darrell <darrellajenkins@gmail.com>
2024-08-08 00:37:47 -0700

    Updated zip maker.

--- zip_maker.py
+++ zip_maker.py
@@ -10,9 +10,12 @@ def create_zip(files, dest_direc_folder):  # You must create the directory folde
 	dest_path = pathlib.Path(dest_direc_folder, "compressed4.zip")  # dest_direc_folder + 'compressed2.zip' name all together into one path name.
 	with zipfile.ZipFile(dest_path, 'w') as zf:
 		for file in files:
-			zf.write(file)
+			file = pathlib.Path(file)  # This along with 'arcname' below makes it so only the file name is in the zip and not sub-folders.
+			zf.write(file, arcname=file.name)
 
 
 if __name__ == '__main__':  # To test the function first within the project's directory.
-	create_zip(files=["goodie.txt", "blackholes.txt", "members.txt", "redhot.txt"], dest_direc_folder="sam")
+	create_zip(files=["goodie.txt", "blackholes.txt", "members.txt", "redhot.txt", "C:/Users/avirt/PycharmProjects/TODO APP/weather.csv"],
+			   dest_direc_folder="sam")
 # Files to be zipped, destination directory folder name you have chosen.
+# If using an absolute path reference, remember to change all "\" to "/".
